1f5dee38bf60ce3b2db18107c85726b6c1958693
yhtiee <99039727+yhtiee@users.noreply.github.com>
2022-11-29 07:03:56 +0100

    render

--- business_software/settings.py
+++ business_software/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t9zl$cdm@4oo7el4!96q$@n7b841m5&*o6^@0wjl+_@v%b)x&w'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-t9zl$cdm@4oo7el4!96q$@n7b841m5&*o6^@0wjl+_@v%b)x&w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -120,11 +125,8 @@ WSGI_APPLICATION = 'business_software.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/business_software', conn_max_age=600)
     }
-}
 
 
 # Password validation
